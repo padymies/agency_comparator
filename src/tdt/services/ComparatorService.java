7e475d740701754c6706bfe01b42cb1c07fc57e4
pady <pady@gmail.com>
2020-10-04 16:45:55 +0200

    Fix exclusions

--- src/tdt/services/ComparatorService.java
+++ src/tdt/services/ComparatorService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
 import tdt.db.dao.IAgencyDao;
 import tdt.db.dao.IAppConfig;
 import tdt.db.dao.IExclusionDao;
@@ -21,7 +22,7 @@ import tdt.model.Note;
 import tdt.model.RateComparator;
 
 public class ComparatorService {
-    
+
     private LoggerService logger;
 
     private final int AGENCY_FORCED = 1;
@@ -39,9 +40,9 @@ public class ComparatorService {
     private IAppConfig configDao;
 
     public ComparatorService() {
-        
+
         logger = new LoggerService("output");
-        
+
         rateDao = new RateImpl();
 
         zoneDao = new ZoneImpl();
@@ -55,152 +56,153 @@ public class ComparatorService {
 
         for (Note note : notes) {
 
-            // ================= 1- SE COMPRUEBA QUE NO ESTÉ FORZADA LA AGENCIA ====================//
-            if (note.getBEST_AGENCY() == null) {
+            ObservableList<AgencyZone> agenciesList = rateDao.getAgenciesByZone(note.getZone().getZoneId());
 
-                ObservableList<AgencyZone> agenciesList = rateDao.getAgenciesByZone(note.getZone().getZoneId());
+            if (!hasExclusions(note, agenciesList)) {
+                // ================= 1- SE COMPRUEBA QUE NO ESTÉ FORZADA LA AGENCIA ====================//
+                if (note.getBEST_AGENCY() == null) {
 
-                checkExclusions(note, agenciesList);
+                    ArrayList<RateComparator> resultList = new ArrayList<>();
 
-                ArrayList<RateComparator> resultList = new ArrayList<>();
+                    if (agenciesList.size() > 0) { // Prueba 
+                        for (AgencyZone agencyZone : agenciesList) {
 
-                if (agenciesList.size() > 0) { // Prueba 
-                    for (AgencyZone agencyZone : agenciesList) {
+                            // Kilo mayor de la tarifa de la agencia_zona
+                            int maxKilos = rateDao.getMaxKilo(agencyZone.getAgencyId(), agencyZone.getZoneId());
 
-                        // Kilo mayor de la tarifa de la agencia_zona
-                        int maxKilos = rateDao.getMaxKilo(agencyZone.getAgencyId(), agencyZone.getZoneId());
+                            double weight = -1;
 
-                        double weight = -1;
-                        
-                        try {
+                            try {
 
-                            weight = Math.ceil(Double.parseDouble(note.getWeight()));
-                            
-                        } catch (NumberFormatException e) {
-                            
-                            AlertExceptionService weightAlert = new AlertExceptionService("Error", "Error en parseo de peso", e);
-                            
-                            weightAlert.showAndWait();
-                        }
+                                weight = Math.ceil(Double.parseDouble(note.getWeight()));
 
-                        RateComparator result = null;
+                            } catch (NumberFormatException e) {
 
-                        if (maxKilos > 0) {
-                            // ================ 2- SE COMPRUEBA QUE NO SOBREPASE EL MAXIMO DE KILOS ==============//
-                            if (weight != -1 && (agencyZone.getMaxKilos() == 0 || agencyZone.getMaxKilos() >= weight)) {
-                                if (maxKilos >= weight) {
+                                AlertExceptionService weightAlert = new AlertExceptionService("Error", "Error en parseo de peso", e);
 
-                                    result = rateDao.ratesNotesCompare(weight, note.getZone().getZoneId(), agencyZone.getAgencyId());
+                                weightAlert.showAndWait();
+                            }
 
-                                } else {
-                                    if (agencyZone.getIncrease() > 0) { // SE APLICA INCREMENTO SI TIENE
+                            RateComparator result = null;
 
-                                        result = rateDao.ratesNotesCompare(maxKilos, note.getZone().getZoneId(), agencyZone.getAgencyId());
+                            if (maxKilos > 0) {
+                                // ================ 2- SE COMPRUEBA QUE NO SOBREPASE EL MAXIMO DE KILOS ==============//
+                                if (weight != -1 && (agencyZone.getMaxKilos() == 0 || agencyZone.getMaxKilos() >= weight)) {
+                                    if (maxKilos >= weight) {
 
-                                        result.setPrice(result.getPrice() + (weight - maxKilos * agencyZone.getIncrease()));
+                                        result = rateDao.ratesNotesCompare(weight, note.getZone().getZoneId(), agencyZone.getAgencyId());
 
                                     } else {
-                                        logger.writeLog("info", "REF: "+ note.getRef() + " ============ NO HAY TARIFA PARA EL PESO  " +  weight + " EN LA ZONA "
-                                    + note.getZone().getName() + " PARA LA AGENCIA "+  agencyZone.getAgencyName() + " ============\n Acción: Revise las tarifas de la zona\n", null);
+                                        if (agencyZone.getIncrease() > 0) { // SE APLICA INCREMENTO SI TIENE
+
+                                            result = rateDao.ratesNotesCompare(maxKilos, note.getZone().getZoneId(), agencyZone.getAgencyId());
+
+                                            result.setPrice(result.getPrice() + (weight - maxKilos * agencyZone.getIncrease()));
+
+                                        } else {
+                                            logger.writeLog("info", "REF: " + note.getRef() + " ============ NO HAY TARIFA PARA EL PESO  " + weight + " EN LA ZONA "
+                                                    + note.getZone().getName() + " PARA LA AGENCIA " + agencyZone.getAgencyName() + " ============\n Acción: Revise las tarifas de la zona\n", null);
+                                        }
+                                    }
+                                }
+                            } else {
+                                logger.writeLog("info", "REF: " + note.getRef() + "============ NO HAY TARIFAS PARA LA AGENCIA  " + agencyZone.getAgencyName() + " EN LA ZONA "
+                                        + note.getZone().getName() + " ============\n Acción: Revise las tarifas de la zona\n", null);
+                            }
+
+                            if (result != null) {
+
+                                resultList.add(result);
+
+                            }
+
+                        }
+                    } else {
+                        logger.writeLog("info", "REF: " + note.getRef() + " ============ NO HAY AGENCIAS PARA LA ZONA:  " + note.getZone().getName() + " ============\n Acción: Revise la Zona\n", null);
+                    }
+                    // ================ 3-COMPROBAMOS EL RESTO DE VARIABLES PARA DETERMINAR EL PRECIO FINAL ================//
+
+                    if (resultList.size() > 0) {
+
+                        resultList.forEach(item -> {
+
+                            if (item.getSurchargeFuel() > 0) {
+
+                                double percent = item.getSurchargeFuel() * item.getPrice() / 100;
+
+                                item.setPrice(item.getPrice() + percent);
+                            }
+
+                            if (note.getRefund() != null) {
+
+                                double refundPrice = 0;
+
+                                double comisionPrice = 0;
+
+                                if (item.getMinimumRefund() > 0) {
+                                    refundPrice = item.getPrice() + item.getMinimumRefund();
+
+                                }
+
+                                if (item.getComision() > 0) {
+
+                                    double percent = item.getPrice() * item.getComision() / 100;
+
+                                    comisionPrice = item.getPrice() + percent;
+                                }
+
+                                if (comisionPrice > 0 && comisionPrice >= refundPrice) {
+                                    item.setPrice(comisionPrice);
+                                }
+
+                                if (refundPrice > 0 && refundPrice > comisionPrice) {
+                                    item.setPrice(refundPrice);
+
+                                }
+
+                            }
+
+                        });
+
+                        Collections.sort(resultList, Comparator.comparing(item -> item.getPrice()));
+
+                        RateComparator first = resultList.get(0);
+
+                        double urgencyPercent = configDao.getUrgencyPercent();
+
+                        double urgencyPrice = 0;
+
+                        urgencyPrice = first.getPrice() + (first.getPrice() * urgencyPercent / 100);
+
+                        if (urgencyPrice > 0) {
+
+                            if (first.getDeliveryTime() > 1) {
+                                for (int i = 1; i < resultList.size(); i++) {
+                                    if (resultList.get(i).getDeliveryTime() < first.getDeliveryTime()
+                                            && resultList.get(i).getPrice() - first.getPrice() < urgencyPrice) {
+
+                                        note.setBEST_AGENCY(resultList.get(i).getAgencyName());
+                                        break;
                                     }
                                 }
                             }
-                        } else {
-                            logger.writeLog("info", "REF: "+ note.getRef() + "============ NO HAY TARIFAS PARA LA AGENCIA  " + agencyZone.getAgencyName() + " EN LA ZONA "
-                                    + note.getZone().getName() + " ============\n Acción: Revise las tarifas de la zona\n", null);
                         }
 
-                        if (result != null) {
-
-                            resultList.add(result);
-
+                        if (note.getBEST_AGENCY() == null) {
+                            note.setBEST_AGENCY(first.getAgencyName());
                         }
 
-                    }
-                } else {
-                    logger.writeLog("info", "REF: "+ note.getRef() +  " ============ NO HAY AGENCIAS PARA LA ZONA:  " + note.getZone().getName() + " ============\n Acción: Revise la Zona\n", null);
-                }
-                // ================ 3-COMPROBAMOS EL RESTO DE VARIABLES PARA DETERMINAR EL PRECIO FINAL ================//
-
-                if (resultList.size() > 0) {
-
-                    resultList.forEach(item -> {
-
-                        if (item.getSurchargeFuel() > 0) {
-
-                            double percent = item.getSurchargeFuel() * item.getPrice() / 100;
-
-                            item.setPrice(item.getPrice() + percent);
-                        }
-
-                        if (note.getRefund() != null) {
-
-                            double refundPrice = 0;
-
-                            double comisionPrice = 0;
-
-                            if (item.getMinimumRefund() > 0) {
-                                refundPrice = item.getPrice() + item.getMinimumRefund();
-
-                            }
-
-                            if (item.getComision() > 0) {
-
-                                double percent = item.getPrice() * item.getComision() / 100;
-
-                                comisionPrice = item.getPrice() + percent;
-                            }
-
-                            if (comisionPrice > 0 && comisionPrice >= refundPrice) {
-                                item.setPrice(comisionPrice);
-                            }
-
-                            if (refundPrice > 0 && refundPrice > comisionPrice) {
-                                item.setPrice(refundPrice);
-
-                            }
-
-                        }
-
-                    });
-
-                    Collections.sort(resultList, Comparator.comparing(item -> item.getPrice()));
-
-                    RateComparator first = resultList.get(0);
-
-                    double urgencyPercent = configDao.getUrgencyPercent();
-
-                    double urgencyPrice = 0;
-
-                    urgencyPrice = first.getPrice() + (first.getPrice() * urgencyPercent / 100);
-
-                    if (urgencyPrice > 0) {
-
-                        if (first.getDeliveryTime() > 1) {
-                            for (int i = 1; i < resultList.size(); i++) {
-                                if (resultList.get(i).getDeliveryTime() < first.getDeliveryTime()
-                                        && resultList.get(i).getPrice() - first.getPrice() < urgencyPrice) {
-
-                                    note.setBEST_AGENCY(resultList.get(i).getAgencyName());
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    if (note.getBEST_AGENCY() == null) {
-                        note.setBEST_AGENCY(first.getAgencyName());
                     }
 
                 }
-
             }
-
         }
 
     }
 
-    private void checkExclusions(Note note, ObservableList<AgencyZone> list) {
+    private boolean hasExclusions(Note note, ObservableList<AgencyZone> list) {
+
+        boolean result = true;
 
         exclusionsDao = new ExclusionsImpl();
 
@@ -232,11 +234,19 @@ public class ComparatorService {
                     }
                     break;
                 default:
-                    logger.writeLog("info", "REF: "+ note.getRef() + " ============ EXCLUSIONES: El código postal " +ex.getPostalCode() + ""
+                    logger.writeLog("info", "REF: " + note.getRef() + " ============ EXCLUSIONES: El código postal " + ex.getPostalCode() + ""
                             + " No se puede enviar por ninguna agencia ============\n Acción: Revise las exclusiones\n", null);
+                    AlertService alert = new AlertService(Alert.AlertType.INFORMATION, "Información de Salida", "Aviso de exclusión:\n"
+                            + "Referencia:  " + note.getRef() + "\nCP  " + note.getDestinationPostalCode() + "", "\n"
+                            + "El código postal está excluido");
+                    alert.show();
                     break;
             }
+        } else {
+            return false;
         }
+
+        return result;
     }
 
 }
